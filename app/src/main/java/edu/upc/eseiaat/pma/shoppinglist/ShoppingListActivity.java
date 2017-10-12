ae031415251e61fb3352b69b6078524642f7cf17
Nuria.ricart <nuria.rs96@gmail.com>
2017-10-12 10:29:21 +0200

    2) Llista creada

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -2,16 +2,44 @@ package edu.upc.eseiaat.pma.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class ShoppingListActivity extends AppCompatActivity {
+
+    private ArrayList<String> item_list;
+    private ArrayAdapter<String> adapter;
+
+    private ListView list;
+    private Button btn_add;
+    private EditText edit_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        list = (ListView) findViewById(R.id.list);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        edit_item = (EditText) findViewById(R.id.edit_item);
+
+        item_list = new ArrayList<>();
+        item_list.add("Potatoes");
+        item_list.add("Garrots");
+        item_list.add("Milk");
+        item_list.add("Cheese");
+
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, item_list);
+        list.setAdapter(adapter);
+
+
 
     }
 }
